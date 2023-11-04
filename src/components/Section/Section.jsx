041744efc41f3d1fe94ad357774875a77bc2ea65
endryuman <132812966+endryuman@users.noreply.github.com>
2023-11-04 16:32:52 +0200

    task 3

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,6 +1,7 @@
+import styles from './Section.module.css';
 export function Section({ title, children }) {
   return (
-    <section>
+    <section className={title ? styles.superSection : styles.baseSection}>
       {title && <h2>{title}</h2>}
       {children}
     </section>
